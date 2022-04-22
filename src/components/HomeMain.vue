0d5a0bde54554ca6198346d601d2e702bf8b2e6c
liangshuang <liangs1104@163.com>
2022-04-22 22:04:21 +0800

    解决：Num变成字符串了 待解决：详情的新闻信息新闻摘要展开和收拢功能不灵敏，需要改变显示框大小才可以。

--- src/components/HomeMain.vue
+++ src/components/HomeMain.vue
@@ -178,7 +178,7 @@ export default {
 
       //分页
       this.proEnterpriseList = proEnterpriseList
-      console.log("proEnterpriseList:",proEnterpriseList)
+      // console.log("proEnterpriseList:",proEnterpriseList)
       this.currentPage = 1
       this.$store.commit('SetLOADING', false)
     },
